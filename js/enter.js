5e85bc74b40ab3759241ed6deab0b1c2e31861d2
Slabnevitch <handofschpilrain@mail.ru>
2024-12-14 18:40:45 +0500

    стили для моб.

--- js/enter.js
+++ js/enter.js
@@ -20,6 +20,6 @@ $('input').each((i,item) => console.log($(item).is(":focus")));
 //   $(this).find('input').eq(0).blur();
 
 // });
-passwordFullModal.show();
+passwordErrorModal.show();
 document.getElementById('open').onclick = () => enterModal.show();
 document.getElementById('close').onclick = () => enterModal.hide();
